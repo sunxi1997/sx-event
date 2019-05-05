d5f401c08fef123d3dc2195942635b92dc3adce5
sunxi1997 <2351405472@qq.com>
2019-05-05 11:07:12 +0800

    更新注释及返回值

--- dist/index.js
+++ dist/index.js
@@ -1,31 +1,81 @@
+const err = (errCode, errMsg) => ({errCode, errMsg});
 
 export default class Event {
-    constructor() {
-        this.hash = {}
-    }
-    $on(eventName, func) {
-        this.hash[eventName] ?
-            this.hash[eventName].push(func):
-            this.hash[eventName] = [func];
-    }
+   constructor() {
+      this.hash = {}
+   }
 
-    $off(eventName, func) {
-        if(this.hash[eventName]){
-            let i = this.hash[eventName].indexOf(func);
-            i!==-1 && this.hash[eventName].splice(i,1)
-        }
+   /**
+    * @method  $on - 绑定一个事件回调
+    *
+    * @param {String} eventName - 要监听的事件名称
+    * @param {Function} func   -  要绑定的回调函数
+    *
+    * @return Object
+    */
+   $on(eventName, func) {
+      // 参数验证
+      if (typeof eventName !== 'string')
+         return err(1, '第一个参数（事件名称）必须为 string')
+      else if (typeof func !== 'function')
+         return err(1, '第二个参数（回调函数）必须为 function')
+      this.hash[eventName] ?
+         this.hash[eventName].push(func) :
+         this.hash[eventName] = [func];
+      return err(0, `为${eventName}绑定了一个回调,${eventName} 事件当前回调总数：${this.hash[eventName].length}`)
+   }
 
-    }
+   /**
+    * @method $off - 取消一个事件的指定回调
+    *
+    * @param {String} eventName - 已监听的事件名称
+    * @param {Function} func   -  要取消的回调函数
+    *
+    * @return Object
+    */
+   $off(eventName, func) {
+      // 参数验证
+      if (typeof eventName !== 'string')
+         return err(1, '第一个参数（事件名称）必须为 string')
+      else if (typeof func !== 'function')
+         return err(1, '第二个参数（回调函数）必须为 function')
+      // 移除监听
+      if (this.hash[eventName]) {
+         let i = this.hash[eventName].indexOf(func);
+         i !== -1 && this.hash[eventName].splice(i, 1);
+         return i !== -1 ?
+            err(0, `为${eventName}事件移除一个监听`) :
+            err(3, `${eventName}事件回调中没有此函数`);
+      } else
+      // 事件不存在
+         return err(2, `没有找到${eventName}事件`)
+   }
 
-    $emit(eventName, ...params) {
-        if(this.hash[eventName])
-            for (let i = 0; i < this.hash[eventName].length; i++) {
-                let func = this.hash[eventName][i];
-                if (func) {
-                    func(...params);
-                    this.hash[eventName][i] !== func && i--
-                }
+   /**
+    * @method $emit
+    *
+    * @param {String} eventName - 要触发的事件名称
+    * @param params   -  触发时传递给回调函数的参数
+    *
+    * @return Object
+    */
+   $emit(eventName, ...params) {
+      // 参数验证
+      if (typeof eventName !== 'string')
+         return err(1, '第一个参数（事件名称）必须为 string')
+      if (this.hash[eventName]) {
+         let i = 0;
+         for (; i < this.hash[eventName].length; i++) {
+            let func = this.hash[eventName][i];
+            if (func) {
+               func(...params);
+               this.hash[eventName][i] !== func && i--
             }
-    }
+         }
+         return err(0, `${eventName}事件完成，共触发${i}个回调`);
+      } else
+      // 事件不存在
+         return err(2, `没有找到${eventName}事件`)
+   }
 
 };
